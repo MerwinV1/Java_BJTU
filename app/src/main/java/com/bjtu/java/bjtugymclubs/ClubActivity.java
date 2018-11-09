95782684841fa79f298e4dbf4ed41412c665add4
MerwinV1 <theo.leguen@epitech.eu>
2018-11-09 18:12:53 +0800

    ss
    Changing clubs

--- app/src/main/java/com/bjtu/java/bjtugymclubs/ClubActivity.java
+++ app/src/main/java/com/bjtu/java/bjtugymclubs/ClubActivity.java
@@ -1,7 +1,12 @@
 package com.bjtu.java.bjtugymclubs;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class ClubActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class ClubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+        CalendarView calendarView= findViewById(R.id.calendarView2);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ClubActivity.this);
+                builder.setMessage("Do you want to subscribe for the course of the "
+                        + month + "/" + dayOfMonth + "/" + year)
+                        .setPositiveButton("Register", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
