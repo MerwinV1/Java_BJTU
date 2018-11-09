b2bbc2f8e434679ce5f84338ce174cd763292393
gregoire <gregoire.descombris@epitech.eu>
2018-11-09 18:28:37 +0800

    list change

--- app/src/main/java/com/bjtu/java/bjtugymclubs/ClubActivity.java
+++ app/src/main/java/com/bjtu/java/bjtugymclubs/ClubActivity.java
@@ -1,7 +1,9 @@
 package com.bjtu.java.bjtugymclubs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ClubActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ClubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("CLUB_NAME");
+
+        TextView club_nameTXT = findViewById(R.id.clubName);
+        club_nameTXT.setText(message);
     }
 }
