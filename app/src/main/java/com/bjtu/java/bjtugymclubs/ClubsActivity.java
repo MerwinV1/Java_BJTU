5aff7301164997c0751a5ad3ae678930d136fbcc
MerwinV1 <theo.leguen@epitech.eu>
2018-11-09 16:50:38 +0800

    Initial commit

---
+++ app/src/main/java/com/bjtu/java/bjtugymclubs/ClubsActivity.java
@@ -0,0 +1,40 @@
+package com.bjtu.java.bjtugymclubs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClubsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clubs);
+
+        String[] clubs = new String[] {
+                "Soccer Club",
+                "Karate Club"
+        };
+
+        final ListView listV = findViewById(R.id.clubsList);
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ClubsActivity.this, ClubActivity.class);
+                startActivity(intent);
+            }
+        });
+        final List<String> clubsList = new ArrayList<String>(Arrays.asList(clubs));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, clubsList);
+        listV.setAdapter(arrayAdapter);
+    }
+}
